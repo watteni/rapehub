f7f23eb16604d7f406f506ed0c2a3f1703c2f12a
paint <111635886+watteni@users.noreply.github.com>
2024-07-07 11:09:41 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,32 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Players = game:GetService("Players")
 
+local Main = Rayfield:CreateWindow({
+    Name = "RapeHub",
+    LoadingTitle = "RapeHub",
+    LoadingSubtitle = "prod RapeSquad",
+    ConfigurationSaving = {
+        Enabled = false,
+        FolderName = nil,
+        FileName = "rapehub"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "HbQeC5hM4V",
+        RememberJoins = true
+    },
+    KeySystem = true,
+    KeySettings = {
+        Title = "Enter your Key",
+        Subtitle = "Key System",
+        Note = "Keys are obtained from @watteni",
+        FileName = "RHauth",
+        SaveKey = true,
+        GrabKeyFromSite = false,
+        Key = {"00pium"}
+    }
+})
+
+local Core = Main:CreateTab("Hub")
+
+local Button = 
